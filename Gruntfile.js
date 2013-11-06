e141fae1763e0d3bd727b3ea2efc642cf1eeb47e
Lee Muhammed <muhammedcode@gmail.com>
2013-11-05 21:05:11 -0800

    Created grunt and bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,21 @@
-module.exports = function (grunt) {
-    grunt.initConfig({
-        // our Grunt task settings
-    });
+module.exports = function(grunt) {
+  grunt.initConfig({
+    jshint: {
+      files: ["Gruntfile.js", "app.js", "public/*.js"]
+    },
+    copy: {
+      main: {
+        files: [
+          {src: ["bower_components/**"], dest: "public/"}
+        ]
+      }
+    },
+    clean: ["bower_components", "public/bower_components", "node_modules"]
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+
+  grunt.registerTask("default", ["jshint","copy"]);
 };
